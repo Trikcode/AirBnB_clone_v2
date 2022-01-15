195af9ad1f42e93f79cd7afd8a0a80cd46e5f7b5
Trikcode <ayesiganobert@gmail.com>
2022-01-15 21:20:34 +0000

    wed

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,29 +1,24 @@
 #!/usr/bin/python3
-"""
-Fabric script based on the file 1-pack_web_static.py that distributes an
-archive to the web servers
-"""
-
-from fabric.api import put, run, env
-from os.path import exists
+""" compress files """
+from fabric.api import *
+import os
 env.hosts = ['35.231.216.93', '35.196.0.213']
 
+
 def do_deploy(archive_path):
-    """distributes an archive to the web servers"""
-    if exists(archive_path) is False:
+    """ decompress files after sending """
+    if not os.path.exists(archive_path):
         return False
     try:
-        file_n = archive_path.split("/")[-1]
-        no_ext = file_n.split(".")[0]
-        path = "/data/web_static/releases/"
-        put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(path, no_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
-        run('rm /tmp/{}'.format(file_n))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
-        run('rm -rf {}{}/web_static'.format(path, no_ext))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        put(archive_path, "/tmp/")
+        filename = archive_path[9:-4]
+        arch_dest = "/data/web_static/releases/" + filename + "/"
+        run("sudo mkdir -p {}".format(arch_dest))
+        run("sudo ls {}".format(arch_dest))
+        run("sudo tar -xvzf {} {}".format(archive_path, arch_dest))
+        run("sudo rm -R {}".format(archive_path))
+        run("sudo ln -sf /data/web_static/releases/{}/ \
+            /data/web_static/current".format(filename))
         return True
     except:
         return False
